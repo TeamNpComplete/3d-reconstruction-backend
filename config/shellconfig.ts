9c7879ba2ce0be9b4045a16365aa196773321e65
Imperial-Dragon <adityagunjal1365@gmail.com>
2021-05-08 00:41:26 +0530

    Added shell config to run python model

---
+++ config/shellconfig.ts
@@ -0,0 +1,5 @@
+export let shellconfig = {
+    reconstructionRunnerCmd : "/mnt/data/Workspace/BE_Project/reconstruction-model/env/bin/python /mnt/data/Workspace/BE_Project/reconstruction-model/testImages.py",
+    uploadDirPath : "/mnt/data/Workspace/BE_Project/reconstruction-backend/uploads",
+    tempDirPath : "/mnt/data/Workspace/BE_Project/reconstruction-backend/temp"
+}
