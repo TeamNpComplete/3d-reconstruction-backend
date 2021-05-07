7accd187e82144f3a0f4620f5c4889468abeeead
Imperial-Dragon <adityagunjal1365@gmail.com>
2021-05-08 00:43:22 +0530

    Added express

---
+++ index.ts
@@ -0,0 +1,23 @@
+import express, { Application, Request, Response } from "express";
+import path from "path";
+import dotenv from 'dotenv';
+
+dotenv.config()
+const app: Application = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended : true}));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers'));
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000 ...');
+})
